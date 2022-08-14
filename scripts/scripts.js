fda8da8f07e150a45baf3cf9879478b22b48f25e
Handouz <mu7ammadsalem@gmail.com>
2022-08-14 11:40:27 +0200

    Initial commit

---
+++ scripts/scripts.js
@@ -0,0 +1,7 @@
+// window.onload=function(){
+//     alert('Window loaded');
+// }
+// $(document).ready(function(){
+//     alert('document loaded');
+// })
+//As we can see in the above code the document is loaded before the window is loaded so we use $(document).ready() to run jQuery scripts.
